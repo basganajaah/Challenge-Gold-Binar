2a57886f19157e8b2f7a29d5c8e1c1da50455693
basganajaah <bandyagaadiansyah@gmail.com>
2023-01-14 09:20:17 +0700

    modified unnecessary code

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,12 +1,12 @@
-import Cookies from "js-cookie";
+// import Cookies from "js-cookie";
 
-const Auth = {
-  isAuthorization: () => (!!Cookies.get("accessToken") ? true : false),
-  getToken: () => Cookies.get("accessToken"),
-  logout: (navigate) => {
-    Cookies.remove("accessToken");
-    navigate("/login");
-  },
-};
+// const Auth = {
+//   isAuthorization: () => (!!Cookies.get("accessToken") ? true : false),
+//   getToken: () => Cookies.get("accessToken"),
+//   logout: (navigate) => {
+//     Cookies.remove("accessToken");
+//     navigate("/login");
+//   },
+// };
 
-export default Auth;
+// export default Auth;
